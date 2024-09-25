81bb9cd097717e27bd5b8c2fad2f94683908c3dc
M. Irfan J <95997939+randkyp@users.noreply.github.com>
2024-09-26 02:41:55 +0700

    part1 ex 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,6 +8,16 @@ const Display = ({ text }) => (
   <div>{text}</div>
 )
 
+const AverageFeedback = ({ good, neutral, bad }) => {
+  const avg = (good + (bad * -1)) / (good + neutral + bad)
+  return <div>average {avg}</div>
+}
+
+const PositiveFeedback = ({ good, neutral, bad }) => {
+  const positive = good / (good + neutral + bad) * 100
+  return <div>positive {positive} %</div>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -22,9 +32,12 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
-      <Display text={`good: ${good}`} />
-      <Display text={`neutral: ${neutral}`} />
-      <Display text={`bad: ${bad}`} />
+      <Display text={`good ${good}`} />
+      <Display text={`neutral ${neutral}`} />
+      <Display text={`bad ${bad}`} />
+      <Display text={`all ${good + neutral + bad}`} />
+      <AverageFeedback good={good} neutral={neutral} bad={bad} />
+      <PositiveFeedback good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
